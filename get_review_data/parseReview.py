63d54d1ff5eafc821c519c454910482d8386db06
hyejin713 <hjsuccess713@gmail.com>
2017-02-06 17:23:19 +0900

    add the module getting review data

---
+++ get_review_data/parseReview.py
@@ -0,0 +1,36 @@
+import json
+import datetime
+
+CONFIG_FILE="./data/Musical_Instruments_5.json"
+CONFIG={}
+now = datetime.datetime.now()
+nowDatetime = now.strftime('%Y%m%d%H%M')
+
+def readConfig(line):
+    js = json.loads(line)
+    repos = js['reviewText']
+    return repos + "\n"
+
+def main() :
+    global CONFIG_FILE
+    global CONFIG
+
+    fw = open("./output/review_only_" + nowDatetime + ".txt", 'a')
+
+    fr = open(CONFIG_FILE, 'r')
+    lines = fr.readlines()
+    for line in lines:
+        #print readConfig(line)
+        fw.write(readConfig(line))
+    fw.close()
+    fr.close()
+
+    #CONFIG = readConfig(CONFIG_FILE)
+
+    #repos = CONFIG['reviewText']
+
+    #print "repos value : " + repos
+
+
+if __name__ == "__main__":
+    main()
