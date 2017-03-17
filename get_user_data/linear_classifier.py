4ddaf4b8295f46775a3c2dda77cb42cdd877c03e
hyejin713 <hjsuccess713@gmail.com>
2017-03-17 19:08:48 +0900

    scenario code uploaded

--- get_user_data/linear_classifier.py
+++ get_user_data/linear_classifier.py
@@ -1,19 +1,59 @@
-import pandas
-from tensorflow.contrib import skflow
+import pandas as pd
+import tensorflow as tf
+from sklearn import metrics, cross_validation
 import random
+import os
+import numpy as np
 
-dirname = '/storage1/data/input/data_per_user/'
-reviewerFolder = os.listdir(dirname)
-for filename in reviewerFolder:
-    full_filename = os.path.join(dirname, filename)
-    if os.path.isdir(full_filename):
-        train = pandas.read_csv(full_filename + '/' + filename + '.csv')
-        
-        random.seed(42)
-        y, X = (train['Survived']>=3?1:0), train[['Age', 'SibSp', 'Fare']].fillna(0)
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-        
-        classifier = skflow.TensorFlowLinearClassifier(n_classes=2, batch_size=128, steps=500, learning_rate=0.05)
-        classifier.fit(X_train, y_train)
-        
-        print(accuracy_score(classifier.predict(X_test), y_test))
+COLUMNS = ["asin","price","salesRank","brand","categories","overall"]
+FEATURES = ["price","overall"]
+LABEL = "overall"
+
+def input_fn(data_set):
+    feature_cols = {k: tf.constant(data_set[k].values) for k in FEATURES}
+    # print(data_set[LABEL].values)
+    labels = tf.constant(data_set[LABEL].values)
+    return feature_cols, labels
+
+dirname = '/storage1/data/output/'
+# reviewerFolder = os.listdir(dirname)
+# for filename in reviewerFolder:
+#     full_filename = os.path.join(dirname, 'A3SP1IWWAGJCAV')
+#     if os.path.isdir(full_filename):
+print("filename: " + "/storage1/data/output/AUCXT9K30SHYF/AUCXT9K30SHYF.csv")
+reviewFileName = "/storage1/data/output/AUCXT9K30SHYF/AUCXT9K30SHYF.csv"
+random.seed(42)
+loadData = pd.read_csv(reviewFileName, skipinitialspace=True,
+                           skiprows=1, header=None, names=COLUMNS)
+loadData['overall'] = (loadData['overall'] >= 3.0).astype(int)
+training_set = loadData[["price","overall"]]
+# print(training_set)
+pd.to_numeric(training_set["price"])
+test_set = loadData[["price","overall"]]
+
+prediction_set = loadData[["price","overall"]]
+
+print(training_set.dtypes)
+print(test_set.dtypes)
+print(prediction_set.dtypes)
+
+feature_cols = [tf.contrib.layers.real_valued_column(k)
+                for k in FEATURES]
+
+regressor = tf.contrib.learn.DNNClassifier(feature_columns=feature_cols,
+                                            hidden_units=[10],
+                                            n_classes=2,
+                                            model_dir="/storage1/tmp")
+regressor.fit(input_fn=lambda: input_fn(training_set), steps=5000)
+
+# Evaluate accuracy.
+ev = regressor.evaluate(input_fn=lambda: input_fn(test_set), steps=1)
+loss_score = ev["loss"]
+print("Loss: {0:f}".format(loss_score))
+
+results = regressor.evaluate(input_fn=lambda: input_fn(test_set), steps=1)
+for key in sorted(results):
+    print "%s: %s" % (key, results[key])
+
+y = regressor.predict(input_fn=lambda: input_fn(prediction_set))
+print ("Predictions: {}".format(str(y)))
